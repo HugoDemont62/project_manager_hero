d73ae29b74bed98fcffde8bacd23b3a9ec7a62b2
HugoDemont <hugodemont62@hotmail.fr>
2025-02-06 17:30:14 +0100

    New style in StartScreen.tsx

--- client/src/components/StartScreen.tsx
+++ client/src/components/StartScreen.tsx
@@ -1,15 +1,19 @@
 import { useGame } from '@/stores/useGame'
 
-export default function StartScreen() {
-  const { startGame } = useGame();
+export default function StartScreen () {
+  const {startGame} = useGame()
 
   return (
     <div className="relative flex flex-col min-h-screen text-white bg-black">
       <div className="absolute inset-0 overflow-hidden">
-        <div className="absolute w-32 h-32 bg-red-500 rounded-full opacity-75 animate-neon-pulse blur-3xl" style={{ top: '10%', left: '20%' }}></div>
-        <div className="absolute w-48 h-48 bg-green-500 rounded-full opacity-75 animate-neon-pulse blur-3xl" style={{ top: '50%', left: '70%' }}></div>
-        <div className="absolute w-24 h-24 bg-blue-500 rounded-full opacity-75 animate-neon-pulse blur-3xl" style={{ top: '70%', left: '30%' }}></div>
-        <div className="absolute w-24 h-24 bg-yellow-500 rounded-full opacity-75 animate-neon-pulse blur-3xl" style={{ top: '5%', left: '80%' }}></div>
+        <div className="absolute w-32 h-32 bg-red-500 rounded-full opacity-75 animate-neon-pulse blur-3xl"
+             style={{top: '10%', left: '20%'}}></div>
+        <div className="absolute w-48 h-48 bg-green-500 rounded-full opacity-75 animate-neon-pulse blur-3xl"
+             style={{top: '50%', left: '70%'}}></div>
+        <div className="absolute w-24 h-24 bg-blue-500 rounded-full opacity-75 animate-neon-pulse blur-3xl"
+             style={{top: '70%', left: '30%'}}></div>
+        <div className="absolute w-24 h-24 bg-yellow-500 rounded-full opacity-75 animate-neon-pulse blur-3xl"
+             style={{top: '5%', left: '80%'}}></div>
       </div>
       <div className="relative grid place-content-center min-h-screen z-10 overflow-hidden">
         <h1 className="text-4xl font-bold mt-8 text-center">Tickets Master</h1>
@@ -25,5 +29,5 @@ export default function StartScreen() {
         </button>
       </div>
     </div>
-  );
+  )
 }
