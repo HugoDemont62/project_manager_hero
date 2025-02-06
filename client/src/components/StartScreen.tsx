8fa12b3261b42f498eec00b4b592a1475361809e
HugoDemont <hugodemont62@hotmail.fr>
2025-02-06 17:31:36 +0100

    style: update StartScreen layout and animations for improved responsiveness

--- client/src/components/StartScreen.tsx
+++ client/src/components/StartScreen.tsx
@@ -6,23 +6,23 @@ export default function StartScreen () {
   return (
     <div className="relative flex flex-col min-h-screen text-white bg-black">
       <div className="absolute inset-0 overflow-hidden">
-        <div className="absolute w-32 h-32 bg-red-500 rounded-full opacity-75 animate-neon-pulse blur-3xl"
+        <div className="absolute w-16 h-16 sm:w-32 sm:h-32 bg-red-500 rounded-full opacity-75 animate-neon-pulse blur-3xl"
              style={{top: '10%', left: '20%'}}></div>
-        <div className="absolute w-48 h-48 bg-green-500 rounded-full opacity-75 animate-neon-pulse blur-3xl"
+        <div className="absolute w-24 h-24 sm:w-48 sm:h-48 bg-green-500 rounded-full opacity-75 animate-neon-pulse blur-3xl"
              style={{top: '50%', left: '70%'}}></div>
-        <div className="absolute w-24 h-24 bg-blue-500 rounded-full opacity-75 animate-neon-pulse blur-3xl"
+        <div className="absolute w-12 h-12 sm:w-24 sm:h-24 bg-blue-500 rounded-full opacity-75 animate-neon-pulse blur-3xl"
              style={{top: '70%', left: '30%'}}></div>
-        <div className="absolute w-24 h-24 bg-yellow-500 rounded-full opacity-75 animate-neon-pulse blur-3xl"
+        <div className="absolute w-12 h-12 sm:w-24 sm:h-24 bg-yellow-500 rounded-full opacity-75 animate-neon-pulse blur-3xl"
              style={{top: '5%', left: '80%'}}></div>
       </div>
-      <div className="relative grid place-content-center min-h-screen z-10 overflow-hidden">
-        <h1 className="text-4xl font-bold mt-8 text-center">Tickets Master</h1>
-        <h2 className="text-l text-gray-400 mt-4 text-center">
+      <div className="relative grid place-content-center min-h-screen z-10 overflow-hidden px-4">
+        <h1 className="text-2xl sm:text-4xl font-bold mt-8 text-center">Tickets Master</h1>
+        <h2 className="text-sm sm:text-lg text-gray-400 mt-4 text-center">
           Tu veux enfin pouvoir gérer tes tickets comme un boss ?<br/>
           Alors, accroche-toi parce que ce jeu est là pour toi !
         </h2>
         <button
-          className="px-6 py-3 text-xl bg-zinc-900 rounded-lg hover:bg-zinc-700 transition-colors mt-4"
+          className="px-4 py-2 sm:px-6 sm:py-3 text-lg sm:text-xl bg-zinc-900 rounded-lg hover:bg-zinc-700 transition-colors mt-4"
           onClick={() => startGame()}
         >
           Start Game
